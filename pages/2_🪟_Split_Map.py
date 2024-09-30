147a1611010af051341f37e30ce6d26c97bdfb19
georanius <138204741+georanius@users.noreply.github.com>
2024-09-30 16:24:36 +0200

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -19,9 +19,9 @@ st.title("Split-panel Map")
 with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map()
-       # m.split_map(
-          #  left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
-       # )
+        m.split_map(
+            left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
+        )
         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 
 m.to_streamlit(height=700)
